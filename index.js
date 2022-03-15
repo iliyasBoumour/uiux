863812fefc23f1728a4e1c3de95b9f2589f2bd25
imadfouad <imadfouad48@gmail.com>
2022-03-15 10:29:36 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { catchErrors, routeNoteFound } = require("./server/errors/error");
+const connecDb = require("./server/models/db");
+dotenv.config();
+const app=express()
+connecDb();
+
+app.use(routeNoteFound);
+app.use(catchErrors);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
---
+++ server/models/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+    console.log("wee");
+  try {
+    await mongoose.connect(process.env.DB_HOST, {
+      keepAlive: 1,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+module.exports = connectDb;
