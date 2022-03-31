0637665bb99a628babef39838b51a59550ead074
iliyasBr <iliyasboumour@gmail.com>
2022-03-31 02:28:12 +0200

    end user pages

--- client/src/pages/Orders/index.jsx
+++ client/src/pages/Orders/index.jsx
@@ -1,7 +1,23 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Store } from "../../utils/Store";
 import axios from "axios";
-
+import {
+  Grid,
+  TableContainer,
+  Table,
+  Typography,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Select,
+  MenuItem,
+  Card,
+  List,
+  ListItem,
+  Button,
+  Container,
+} from "@mui/material";
 const Index = () => {
   const [orders, setOrders] = useState([]);
   const { state } = useContext(Store);
@@ -22,24 +38,31 @@ const Index = () => {
   }, []);
 
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Order id</th>
-            <th>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((o) => (
-            <tr key={o._id}>
-              <td>{o._id}</td>
-              <td>{o.valid ? "validated" : "pending"}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <Container sx={{ padding: "2rem" }}>
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Order id</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((item) => (
+              <TableRow key={item._id}>
+                <TableCell>
+                  <Typography style={{ color: "black" }}>{item._id}</Typography>
+                </TableCell>
+
+                <TableCell align="right">
+                  {item.valid ? "validated" : "pending"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
